c2a110387e3850b83eaefe91c11fbaaeee6e15b2
huzaifa311 <119949757+huzaifa311@users.noreply.github.com>
2023-05-05 22:20:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//                               \ALERTS
+
+/*Question no 01: Write a script to greet your website visitor using JS alert box.*/
+var username = prompt("Enter Your Name")
+alert("Assalamualikum " + username)
+
+/*Question no 02: Write a script to display following message on your web page:*/
+alert("Error! Please enter a valid password");
+
+/*Question no 03: Write a script to display following message on your web page: (Hint : Use line break)*/
+alert("Welcome to JS land... \nHappy Coding!");
+
+/*Question no 04: Write a script to display following messages in sequence:*/
+alert("Welcome to JS land...")
+
+/*Question no 05: Generate the following message through browser’s developer console:*/
+alert("Hello... I can run JS through my web browser's console ")
+
+
+
+
+
+
+
